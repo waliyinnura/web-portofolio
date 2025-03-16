1eea95224402c7f6bdf10a22c983e192c950b9ab
Weli <waliyinnura@gmail.com>
2025-03-16 11:03:03 +0800

    chore: update copyright year

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {NavigationDots, SocialMedia} from '../components'
 
+const year = new Date().getFullYear()
 const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
     <div id={idName} className={`app__container ${classNames}`}>
@@ -8,7 +9,7 @@ const AppWrap = (Component, idName, classNames) => function HOC() {
         <div className='app__wrapper app_flex'>
             <Component/>
             <div className='copyright'>
-                <p className='p-text'>@2024 WELI</p>
+                <p className='p-text'>@{year} WELI</p>
                 <p className='p-text'>All right reserved</p>
             </div>
         </div>
